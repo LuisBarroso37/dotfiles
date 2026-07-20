e9c758ab03ade9c2c2af73bf398e50bf34898c86
LuisBarroso37 <58770446+LuisBarroso37@users.noreply.github.com>
2026-07-20 15:16:53 +0100

    feat(nvim): add keymaps to yank file path to clipboard
    <leader>yp yanks the relative path, <leader>yP the absolute path.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,3 +15,13 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 -- Delete to the black hole register (delete without saving to a register)
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- Yank relative path to system clipboard
+vim.keymap.set("n", "<leader>yp", function()
+  vim.fn.setreg("+", vim.fn.expand("%"))
+end, { desc = "Yank relative path" })
+
+-- Yank absolute path
+vim.keymap.set("n", "<leader>yP", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, { desc = "Yank absolute path" })
