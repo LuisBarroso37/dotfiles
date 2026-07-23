b102419b836b5cdcc1c7787dcd88560e6a9201e4
LuisBarroso37 <58770446+LuisBarroso37@users.noreply.github.com>
2026-07-23 16:44:53 +0200

    fix(nvim): add render-markdown dependencies

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -4,6 +4,7 @@ return {
   -- Toggle with <leader>um.
   {
     "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
     ft = { "markdown" },
     opts = {
       render_modes = { "n", "c", "t" },
